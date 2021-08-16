956880b17280f3dc7425a9ac357a25f38b3c1447
Bhavik Maneck <bmaneck@gmail.com>
2021-08-17 00:58:42 +1000

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+
+
+def get_hashtags_to_monitor():
+    return ['#photostreamdemo', '#trent100']
+
+
+def main():
+    hashtags_to_monitor = get_hashtags_to_monitor()
+
+    # every x minutes loop:
+
+    # for each hashtag
+
+    # get latest posts from hashtag
+
+    # if post ID not in cache (Redis or advise otherwise)
+
+    # process the post and post to URL
+    # process_post(ig_post_data)
+
+    # save processed post IDs to cache
+    pass
+
+
+def process_post(ig_post_data):
+    # once we have some sample responses from the API lets discuss the processing steps
+    return {}
+
+
+if __name__ == '__main__':
+    main()
